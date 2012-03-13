ae4c7dbaa90fbbd70fa086096ad7f5be5e709859
Ondřej Machulda <ondrej.machulda@gmail.com>
2012-03-13 13:44:59 +0100

    Coding style improved

--- fnc.php
+++ fnc.php
@@ -6,12 +6,12 @@
  */
 function d($var)
 {
-  echo '<div style="background:#f8f8f8;margin:5px;padding:5px;border: solid grey 1px;">'."\r\n";
-  echo dtrace();
-  echo '<pre style="margin:0px;padding:0px;">'."\r\n";
-  var_dump($var);
-  echo '</pre>'."\r\n";
-  echo '</div>'."\r\n";
+    echo '<div style="background:#f8f8f8;margin:5px;padding:5px;border: solid grey 1px;">'."\r\n";
+    echo dtrace();
+    echo '<pre style="margin:0px;padding:0px;">'."\r\n";
+    var_dump($var);
+    echo '</pre>'."\r\n";
+    echo '</div>'."\r\n";
 }
 
 /**
@@ -21,13 +21,13 @@ function d($var)
  */
 function dd($var)
 {
-  echo '<div style="background:#fafafa;margin:5px;padding:5px;border: solid grey 1px;">'."\r\n";
-  echo dtrace();
-  echo '<pre style="margin:0px;padding:0px;">'."\r\n";
-  var_dump($var);
-  echo '</pre>'."\r\n";
-  echo '</div>'."\r\n";
-  die();
+    echo '<div style="background:#fafafa;margin:5px;padding:5px;border: solid grey 1px;">'."\r\n";
+    echo dtrace();
+    echo '<pre style="margin:0px;padding:0px;">'."\r\n";
+    var_dump($var);
+    echo '</pre>'."\r\n";
+    echo '</div>'."\r\n";
+    die();
 }
 
 /**
@@ -37,12 +37,12 @@ function dd($var)
  */
 function ds($var)
 {
-  echo '<div style="background:#fafafa;margin:5px;padding:5px;border: solid grey 1px;">'."\r\n";
-  echo dtrace();
-  echo '<pre style="margin:0px;padding:0px;">'."\r\n";
-  var_dump((string) $var);
-  echo '</pre>'."\r\n";
-  echo '</div>'."\r\n";
+    echo '<div style="background:#fafafa;margin:5px;padding:5px;border: solid grey 1px;">'."\r\n";
+    echo dtrace();
+    echo '<pre style="margin:0px;padding:0px;">'."\r\n";
+    var_dump((string) $var);
+    echo '</pre>'."\r\n";
+    echo '</div>'."\r\n";
 }
 
 /**
@@ -52,13 +52,13 @@ function ds($var)
  */
 function dsd($x)
 {
-  echo '<div style="background:#fafafa;margin:5px;padding:5px;border: solid grey 1px;">'."\r\n";
-  echo dtrace();
-  echo '<pre style="margin:0px;padding:0px;">'."\r\n";
-  var_dump((string) $var);
-  echo '</pre>'."\r\n";
-  echo '</div>'."\r\n";
-  die();
+    echo '<div style="background:#fafafa;margin:5px;padding:5px;border: solid grey 1px;">'."\r\n";
+    echo dtrace();
+    echo '<pre style="margin:0px;padding:0px;">'."\r\n";
+    var_dump((string) $var);
+    echo '</pre>'."\r\n";
+    echo '</div>'."\r\n";
+    die();
 }
 
 /**
@@ -83,31 +83,32 @@ function dmem()
  * @param type $label
  */
 function dtimer(&$timers, $status = 0, $label = null)
-{ if (!is_array($timers) || $status === -1) {
-    $timers = array();
-  }
-  $where = dtrace();
-  if(null !== $label){
-    $where = $label . ' - ' . $where;
-  }
-
-
-  $timers[] = array('where' => $where, 'time' => microtime(true));
-  if ($status === 1) {
-    echo '<table style="border-color: black;" border="1" cellpadding="3" cellspacing="0">';
-    echo '<tr style="background-color:black;color:white;"><th>Trace</th><th>dT</th><th>dT(cumm)</th></tr>';
-    $lastTime = $timers[0]['time'];
-    $firstTime = $timers[0]['time'];
-    foreach ($timers as $timer) {
-      echo sprintf('<tr><td>%s</td><td>%s</td><td>%s</td></tr>',
-        $timer['where'],
-        sprintf('%01.6f',round($timer['time'] - $lastTime,6)),
-        sprintf('%01.6f',round($timer['time'] - $firstTime,6))
-      );
-      $lastTime = $timer['time'];
+{
+    if (!is_array($timers) || $status === -1) {
+        $timers = array();
     }
-    echo '</table>';
-  }
+    $where = dtrace();
+    if (null !== $label){
+        $where = $label . ' - ' . $where;
+    }
+
+
+    $timers[] = array('where' => $where, 'time' => microtime(true));
+    if ($status === 1) {
+        echo '<table style="border-color: black;" border="1" cellpadding="3" cellspacing="0">';
+        echo '<tr style="background-color:black;color:white;"><th>Trace</th><th>dT</th><th>dT(cumm)</th></tr>';
+        $lastTime = $timers[0]['time'];
+        $firstTime = $timers[0]['time'];
+        foreach ($timers as $timer) {
+            echo sprintf('<tr><td>%s</td><td>%s</td><td>%s</td></tr>',
+                $timer['where'],
+                sprintf('%01.6f',round($timer['time'] - $lastTime,6)),
+                sprintf('%01.6f',round($timer['time'] - $firstTime,6))
+            );
+            $lastTime = $timer['time'];
+        }
+        echo '</table>';
+    }
 }
 
 /**
@@ -117,17 +118,17 @@ function dtimer(&$timers, $status = 0, $label = null)
  */
 function dtrace()
 {
-  $bt = debug_backtrace();
-  $trace = $bt[1];
-  $line = $trace['line'];
-  $file = basename($trace['file']);
-  $function = $trace['function'];
-  $class = (isset($bt[2]['class'])?$bt[2]['class']:basename($trace['file']));
-  if (isset($bt[2]['class'])) {
-    $type = $bt[2]['type'];
-  } else {
-    $type = ' ';
-  }
-  $function = isset($bt[2]['function']) ? $bt[2]['function'] : '';
-  return sprintf("%s%s%s() line %s <small>(in %s)</small>",$class, $type, $function, $line, $file);
+    $bt = debug_backtrace();
+    $trace = $bt[1];
+    $line = $trace['line'];
+    $file = basename($trace['file']);
+    $function = $trace['function'];
+    $class = (isset($bt[2]['class'])?$bt[2]['class']:basename($trace['file']));
+    if (isset($bt[2]['class'])) {
+        $type = $bt[2]['type'];
+    } else {
+        $type = ' ';
+    }
+    $function = isset($bt[2]['function']) ? $bt[2]['function'] : '';
+    return sprintf("%s%s%s() line %s <small>(in %s)</small>",$class, $type, $function, $line, $file);
 }
